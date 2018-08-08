b378cc276768a67429281081fc2ff773b835aa44
Jiaming Lou <stylerlight101@gmail.com>
2018-08-08 17:04:47 +0000

    automatically backed up by learn

---
+++ trycode.rb
@@ -0,0 +1,19 @@
+# def hungry_mn
+#   kitkat = 0
+#   while kitkat < 30
+#   puts "Give me another kitkat!"
+#   kitkat += 1
+# end
+# end
+
+# puts hungry_mn
+
+def hungry_gp
+  mango = 0
+  until mango == 3
+  puts "GIVE ME MY MANGO!"
+  mango += 1
+end
+end
+
+puts hungry_gp
